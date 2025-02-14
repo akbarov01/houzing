3251c00cd3abb846204ade73bc9fe5c159c8aaf6
akbarov01 <hojiakbarakbarov851@gmail.com>
2025-02-14 18:05:19 +0500

    carousel created

---
+++ review.js
@@ -0,0 +1,15 @@
+// 1-dars
+// Project Yasash 
+// Project strukturalash (pattern)
+// react-router-dom@6 ni konfiguratsiya qilish
+// Generic Navbar yasash
+// useID()
+// Netlify ga deploy qilish
+
+// 2-dars
+// Netlify auto deploy via github branch
+// Generic Button, Input
+// forwardRef()
+
+// 3-dars
+// Carousel
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import {Input} from "../Generic";
-import { Container } from "./style";
+import  Carousel  from '../Carousel';
+import  {Container}  from "./style";
+
 
 export const Home = () => {
   return (
     <Container>
-      <h1>Home</h1>
-      <Input width={200} placeholder={'test'} />
+      <Carousel />
     </Container>
   );
 };
